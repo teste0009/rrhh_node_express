bc985f6287d1314688e3e038bc1c3f4d9a73b923
teste0009 <maidentallica@gmail.com>
2022-06-15 00:16:11 -0300

    Pass vars to Template

--- routes/tests.js
+++ routes/tests.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET Tests page. */
 router.get('/', function(req, res, next) {
-  // locale.name = 'Enzo'; // res.render('tests', { title: 'Mundo', name: 'Enzo' });
-  res.send('respond with a resource');
+  // locale.name = 'Enzo';
+  res.render('tests', { title: 'Mundo', name: 'Enzo', animals: ['Cat', 'Dog', 'Wolf'] });
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
