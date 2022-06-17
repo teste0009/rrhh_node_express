5af939fc4b345795f5737a2ebac9ffff62cb5930
teste0009 <maidentallica@gmail.com>
2022-06-17 07:14:57 -0300

    Template and css

--- routes/tests.js
+++ routes/tests.js
@@ -4,16 +4,18 @@ var router = express.Router();
 /* GET Tests page. */
 router.get('/', function(req, res, next) {
 
+  let [title, name, animals] = ['Mundo', 'Enzo', ['Cat', 'Dog', 'Wolf']]
+
   const {ciudades, departamentos, hola} = require('../controller/tests.js');
 
   Promise.all([ciudades, departamentos])
     .then( results => { // console.log(results);
       const [ciudades, departamentos] = results; // console.log(ciudades);
-      res.render('tests', { title: 'Mundo', name: 'Enzo', animals: ['Cat', 'Dog', 'Wolf'], ciudades, departamentos, saludo: hola, error: false });
+      res.render('tests', { title, name, animals, ciudades, departamentos, saludo: hola, error: false });
     })
     .catch( error => {
       // console.log(error);
-      res.render('tests', { title: 'Mundo', name: 'Enzo', animals: ['Cat', 'Dog', 'Wolf'], ciudades: false, departamentos: false, saludo: hola, error: error });
+      res.render('tests', { title, name, animals, ciudades: false, departamentos: false, saludo: hola, error: error });
     })
     .finally( () => {
       console.log('Finally!');
